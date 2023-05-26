d4c7989d4ef3b1d05aa7ee4a84b9a74c76dbadff
george-mountain <georgejiokem@gmail.com>
2023-05-26 18:10:49 +0900

    add schema and documentation

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,51 @@
+"""
+------------------------------
+USING GENERIC URLS
+
+In using the generic urls, we will have a url for each end point of our project.
+
+Every endpoint of the api, will have a separate url.
+
+-----------------------------------
+"""
+
+
+# from django.urls import path
+# from .views import PostList, PostDetail, UserList, UserDetail  # new
+
+
+# urlpatterns = [
+#     path("users/", UserList.as_view()),  # new
+#     path("users/<int:pk>/", UserDetail.as_view()),  # new
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+#     path("", PostList.as_view(), name="post_list"),
+# ]
+
+
+"""
+------------------------------
+USING ROUTER FOR URLS
+
+Routers work directly with viewsets to automatically generate URL patterns
+for us. 
+
+We will use SimpleRouter but it’s also possible to create
+custom routers for more advanced functionality
+
+-----------------------------------
+"""
+
+
 from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter
 
 
-urlpatterns = [
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+from .views import UserViewSet, PostViewSet
+
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+
+urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,79 @@
-from rest_framework import generics, permissions  # new
+"""
+-------------------------------------------------------
+Using Generic views for the views.
+
+In this case, we will write a single view for each object
+
+For example, for user view, we will two views which are:
+UserList view and UserDetail view.
+
+The same is applicable to posts etc.
+-------------------------------------------------------
+
+using viewsets
+"""
+
+# from django.contrib.auth import get_user_model  # new
+# from rest_framework import generics, permissions  # new
+
+# from .models import Post
+# from .permissions import IsAuthorOrReadOnly  # new
+# from .serializers import PostSerializer, UserSerializer  # new
+
+
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)  # new
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     # permission_classes = (permissions.IsAdminUser,)  # new
+#     permission_classes = (IsAuthorOrReadOnly,)  # new
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class UserList(generics.ListCreateAPIView):  # new
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):  # new
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+"""
+------------------------------------
+Using Viewset for the views.
+
+In this case, we just combine the related views
+
+into one view. For example, all user views are 
+related and all post views are related.
+
+Instead of having these vies as separate views, we can combine them
+
+using viewsets
+"""
+
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets  # new
+from rest_framework.permissions import IsAdminUser  # new
 
 from .models import Post
-from .permissions import IsAuthorOrReadOnly  # new
-from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)  # new
+class PostViewSet(viewsets.ModelViewSet):  # new
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = (permissions.IsAdminUser,)  # new
-    permission_classes = (IsAuthorOrReadOnly,)  # new
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):  # new
+    permission_classes = [IsAdminUser]  # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
